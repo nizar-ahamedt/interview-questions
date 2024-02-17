7f2152e394b1560331ecb442d4c04a764654d3ed
nizar-ahamedt <nizarahamedt@gmail.com>
2024-02-18 01:08:49 +0530

    file Explorer ui

---
+++ front-interview/react-interview-coding-question/FileExplorer/src/data/folderData.js
@@ -0,0 +1,90 @@
+export const  explorer = {
+    id: 1,
+    name: "root",
+    isfolder: true,
+    items: [
+
+        {
+            id: 2,
+            name: "public",
+            isfolder: true,
+            items: [
+                {
+                    id: 3,
+                    name: "public-nested-1",
+                    isfolder: true,
+                    items: []
+
+
+
+
+                }
+
+
+            ]
+        },
+        {
+            id: 4,
+            name: "src",
+            isfolder: true,
+            items: [
+                {
+                    id: 10,
+                    name: "components",
+                    isfolder: true,
+                    items: [
+
+
+
+                    ]
+                },
+
+                {
+                    id: 5,
+                    name: "App.jsx",
+                    isfolder: false,
+                    items: [
+
+
+
+                    ]
+                },
+                {
+                    id: 6,
+                    name: "index.jsx",
+                    isfolder: false,
+                    items: [
+
+
+
+                    ]
+                },
+                {
+                    id: 7,
+                    name: "main.jsx",
+                    isfolder: false,
+                    items: [
+
+
+
+                    ]
+                }
+
+            ]
+        },
+        {
+            id: 8,
+            name: "assets",
+            isfolder: true,
+            items: [
+
+
+
+            ]
+        }
+
+    ]
+
+
+
+}
